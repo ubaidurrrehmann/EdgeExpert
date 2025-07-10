1b4e39555f2316ebfb034b1f8c0d734cdcbc02c5
Ubaid <ubaid.rehman@dimensionalsys.com>
2025-07-11 04:43:55 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Sticky Navbar */}
+      {/* Sticky Navbar */}
       <Navbar />
 
       {/* Main Content */}
